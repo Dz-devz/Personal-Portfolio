1b420cc9050fe1cfaf808f16f1767629084bad34
Dz.dev <darwinbjordan@gmail.com>
2024-07-20 00:03:57 +0800

    progress: added header and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import "./App.css";
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/button";
+import Header from "./components/header";
 
-function App() {
+export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello this is the first step to a portfolio journey
-      </h1>
+      <h1 className="text-3xl font-bold underline">Dz.dev</h1>
 
       <div className="pt-[4.7rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10">Something</Button>
+        <Header />
       </div>
 
       <ButtonGradient />
     </>
   );
 }
-
-export default App;
