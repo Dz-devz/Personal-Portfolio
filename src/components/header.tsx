35fc3803cfbfd4aa3c57e487580578a7f1453fee
Dz.dev <darwinbjordan@gmail.com>
2024-07-20 09:08:27 +0800

    progress: delete unnecessary element in tailwind config and bring back postcss to jsto eliminate issue with type: module

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,10 @@
+import { useLocation } from "react-router-dom";
+
 import dzdev from "../assets/dzdev.svg";
 import { navigation } from "../constants";
 
 function Header() {
+  const pathName = useLocation();
   return (
     <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
       <div className="flex item-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
@@ -14,7 +17,11 @@ function Header() {
               <a
                 key={nav.id}
                 href={nav.url}
-                className={`block relative font-code text-2xl uppeercase text-n-1 trnsition-colors hover:text-color-1 px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold`}
+                className={`block relative font-code text-2xl uppeercase text-n-1 trnsition-colors hover:text-color-1 px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                  nav.url === pathName.hash
+                    ? "z-2 lg:text-n-1"
+                    : "lg:text-n-1/50"
+                }`}
               >
                 {nav.title}
               </a>
