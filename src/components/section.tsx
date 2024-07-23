b14f8987475c6f2ee65773d6004534aea43666f6
Dz-devz <darwinbjordan@gmail.com>
2024-07-23 19:31:39 +0800

    chore: fix section svg crosses not aligned and mispelled and missing tailwind

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -5,7 +5,7 @@ interface SectionProps {
   id?: string;
   crosses?: boolean;
   crossesOffset?: string;
-  customPadding?: string;
+  customPaddings?: string;
   children: React.ReactNode;
 }
 
@@ -14,30 +14,31 @@ export default function Section({
   id,
   crosses,
   crossesOffset,
-  customPadding,
+  customPaddings,
   children,
 }: SectionProps) {
   return (
     <div
       id={id}
-      className={`relative ${
-        customPadding ||
-        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""} ${
-          className || ""
-        }`
-      }`}
+      className={`
+      relative 
+      ${
+        customPaddings ||
+        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+      } 
+      ${className || ""}`}
     >
       {children}
 
       <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10" />
-      <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10" />
+      <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10" />
 
       {crosses && (
         <>
           <div
             className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 ${
               crossesOffset && crossesOffset
-            }pointer-events-none lg:block xl:left-10 right-10`}
+            } pointer-events-none lg:block xl:left-10 right-10`}
           />
           <SectionSvg crossesOffset={crossesOffset} />
         </>
