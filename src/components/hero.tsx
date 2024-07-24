5db3efb0750400a454cb6e05957494afc8f772d7
Dz-devz <darwinbjordan@gmail.com>
2024-07-24 15:13:40 +0800

    progress: responsive image and container

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -23,8 +23,14 @@ export default function Hero() {
           <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
             <div className="relative bg-n-8 rounded-[1rem]">
               <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
-              <div>
-                <img src={DzDP} />
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                <img
+                  src={DzDP}
+                  className="w-full"
+                  width={1440}
+                  height={1800}
+                  alt="Display Picture of Dz dev"
+                />
               </div>
             </div>
           </div>
