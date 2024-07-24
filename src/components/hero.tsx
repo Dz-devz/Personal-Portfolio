141d4c15e5856b004488a8d461e567b36584e45a
Dz-devz <darwinbjordan@gmail.com>
2024-07-24 15:10:30 +0800

    progress: added container for hero section image

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,6 @@
 import Button from "./button";
 import Section from "./section";
+import DzDP from "../assets/DzDP.jpg";
 
 export default function Hero() {
   return (
@@ -17,6 +18,16 @@ export default function Hero() {
             project.
           </h1>
           <Button href="/projects">Explore now</Button>
+        </div>
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+            <div className="relative bg-n-8 rounded-[1rem]">
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+              <div>
+                <img src={DzDP} />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Section>
